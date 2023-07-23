288df024780b18ad0e43c98749d108ffc27d522a
AzorAha1 <wizfaiz@icloud.com>
2023-07-23 07:47:21 +0100

    printf

--- printf.c
+++ printf.c
@@ -22,13 +22,12 @@ void print_char(va_list arguments)
  * Description - this is the description
  * Return: return type
  */
-void print_int(va_list arguments)
+void print_int(va_list arguments, char *output, int *length)
 {
 	int i = va_arg(arguments, int);
-	char output[40];
-	int length = sprintf(output, "%d", i);
+	*length = sprintf(output, "%d", i);
 
-	write(1, output, length);
+	write(1, output, *length);
 }
 /**
  * print_double - function name
@@ -36,13 +35,12 @@ void print_int(va_list arguments)
  * Description - this is the description
  * Return: return type
  */
-void print_double(va_list arguments)
+void print_double(va_list arguments, char *output, int *length)
 {
 	double flo = va_arg(arguments, double);
-	char output[40];
-	int length = sprintf(output, "%f", flo);
+	*length = sprintf(output, "%f", flo);
 
-	write(1, output, length);
+	write(1, output, *length);
 }
 /**
  * print_string - function name
@@ -50,18 +48,19 @@ void print_double(va_list arguments)
  * Description - this is the description
  * Return: return type
  */
-void print_string(va_list arguments)
+void print_string(va_list arguments, char *output, int *length)
 {
 	char *ch = va_arg(arguments, char*);
-	char output[40];
-	int length = sprintf(output, "%s", ch);
 
 	if (ch == NULL)
 	{
-		printf("(nil)");
-		return;
+		*length = write(1, "(nil)", 5);
 	}
-	write(1, output, length);
+	else
+	{
+		*length = sprintf(output, "%s", ch);
+		write(1, output, *length);
+	}
 }
 /**
  * print_all - function name
@@ -74,7 +73,10 @@ int _printf(const char *format, ...)
 	va_list arguments;
 	int i;
 	int format_l;
+	char output[40];
+	int length;
 
+	length = 0;
 	format_l = strlen(format);
 	va_start(arguments, format);
 	for (i = 0; i < format_l; i++)
@@ -88,31 +90,32 @@ int _printf(const char *format, ...)
 			}
 			else if (format[i] == 'c')
 			{
-				print_char(arguments);
+				char c = va_arg(arguments, int);
+
+				length = write(1, &c, 1);
 			}
 			else if (format[i] == 's')
 			{
-				print_string(arguments);
+				print_string(arguments, output, &length);
 			}
 			else if (format[i] == 'd')
 			{
-				print_int(arguments);
+				print_int(arguments, output, &length);
 			}
 			else if (format[i] == 'f')
 			{
-				print_double(arguments);
+				print_double(arguments, output, &length);
 			}
 			else if (format[i] == 'r')
 			{
-				putchar('%');
-				putchar('r');
+				length = write(1, "%r", 2);
 			}
 		}
 		else
 		{
-			putchar(format[i]);
+			length =  write(1, &format[i], 1);
 		}
 	}
 	va_end(arguments);
-	return (0);
+	return (length);
 }
