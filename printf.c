8cdefb7af0a4e7d800746d2a1323768e8834079d
AzorAha1 <wizfaiz@icloud.com>
2023-07-25 00:53:04 +0100

    printf.c

--- printf.c
+++ printf.c
@@ -4,17 +4,13 @@
 #include <string.h>
 #include <unistd.h>
 #include "main.h"
+#include <stdint.h>
 /**
  * print_char - function name
  * @arguments: takes arguments
  * Description - this is the description
  * Return: return type
  */
-void print_r(va_list arguments)
-{
-	(void)arguments;
-	write(1, "%r", 2);
-}
 void print_char(va_list arguments)
 {
 	char c;
@@ -32,7 +28,7 @@ void print_int(va_list arguments)
 {
 	int i = va_arg(arguments, int);
 	char output[BUFFER_SIZE];
-	int length = sprintf(output, "%d", i);
+	int length = snprintf(output,BUFFER_SIZE, "%d", i);
 
 	write(1, output, length);
 }
@@ -46,8 +42,8 @@ void print_double(va_list arguments)
 {
 	double flo = va_arg(arguments, double);
 	char output[BUFFER_SIZE];
-	int length = sprintf(output, "%f", flo);
-
+	int length = snprintf(output, BUFFER_SIZE, "%f", flo);
+	
 	write(1, output, length);
 }
 /**
@@ -59,8 +55,99 @@ void print_double(va_list arguments)
 void print_string(va_list arguments)
 {
 	char *ch = va_arg(arguments, char*);
+
+	if (ch == NULL)
+	{
+		write(1, "(null)", 1);
+		return;
+	}
 	write(1, ch, strlen(ch));
 }
+/**
+ * print_u - function name
+ * @arguments: parameters
+ * Description - this is the description
+ * Return: return type
+ */
+void print_u(va_list arguments)
+{
+	unsigned int u = va_arg(arguments, unsigned int);
+	char output[BUFFER_SIZE];
+	int length = snprintf(output, BUFFER_SIZE, "%u", u);
+
+	write(1, output, length);
+}
+/**
+ * print_o - function name
+ * @argument: parameter
+ * Description - this is the description
+ * Return: return type
+ */
+void print_o(va_list arguments)
+{
+	unsigned int o = va_arg(arguments, unsigned int);
+	char output[BUFFER_SIZE];
+	int length = snprintf(output, BUFFER_SIZE, "%o", o);
+
+	write(1, output, length);
+}
+/**
+ * print_x - function name
+ * @argument: parameter
+ * Description - this is the description
+ * Return: return type
+ */
+void print_x(va_list arguments)
+{
+	unsigned int x = va_arg(arguments, unsigned int);
+	char output[BUFFER_SIZE];
+	int length = snprintf(output, BUFFER_SIZE, "%x", x);
+	
+	write(1, output, length);
+}
+/**
+ * print_hex - function name
+ * @argument: parameter
+ * Description - this is the description
+ * Return: return type
+ */
+void print_hex(va_list arguments)
+{
+        unsigned int x = va_arg(arguments, unsigned int);
+        char output[BUFFER_SIZE];
+        int length = snprintf(output, BUFFER_SIZE, "%X", x);
+
+        write(1, output, length);
+}
+/**
+ * print_percent - function name
+ * @argument: parameter
+ * Description - this is the description
+ * Return: return type
+ */
+void print_percent(va_list arguments)
+{
+	char c;
+
+        (void)arguments;
+	c = '%';
+        write(1, &c, 1);
+}
+/**
+ * print_address - function name
+ * @argument: parameter
+ * Description - this is the description
+ * Return: return type
+ */
+void print_address(va_list arguments)
+{
+        void *p = va_arg(arguments, void *);
+	char output[BUFFER_SIZE];
+	int length = snprintf(output, BUFFER_SIZE, "%p", p);
+
+	write(1, output, length);
+}
+
 /**
  * _printf - function name
  * @format: takes cahr
@@ -72,7 +159,9 @@ int _printf(const char *format, ...)
 	va_list arguments;
 	int i;
 	int format_l;
+	int length;
 
+	length = 0;
 	format_l = strlen(format);
 	va_start(arguments, format);
 	for (i = 0; i < format_l; i++)
@@ -92,7 +181,7 @@ int _printf(const char *format, ...)
 			{
 				print_string(arguments);
 			}
-			else if (format[i] == 'd')
+			else if (format[i] == 'd' || format[i] == 'i')
 			{
 				print_int(arguments);
 			}
@@ -100,9 +189,29 @@ int _printf(const char *format, ...)
 			{
 				print_double(arguments);
 			}
-			else if (format[i] == 'r')
+			else if (format[i] == 'u')
 			{
-				print_r(arguments);
+				print_u(arguments);
+			}
+			else if (format[i] == 'o')
+			{
+				print_o(arguments);
+			}
+			else if (format[i] == 'x')
+			{
+				print_x(arguments);
+			}
+			else if (format[i] == 'X')
+			{
+				print_hex(arguments);
+			}
+			else if (format[i] == '%')
+			{
+				print_percent(arguments);
+			}
+			else if (format[i] == 'p')
+			{
+				print_address(arguments);
 			}
 			else
 			{
@@ -116,5 +225,5 @@ int _printf(const char *format, ...)
 		}
 	}
 	va_end(arguments);
-	return (0);
+	return (length);
 }
